cf152e2708c9f28b68e78994d794c5ed49c20b34
ak985 <ak985@njit.edu>
2017-12-21 13:19:47 -0500

    added updateTodo function

--- model/todosDB.php
+++ model/todosDB.php
@@ -21,3 +21,17 @@ function deleteTodo($id) {
     $statement->execute();
     $statement->closeCursor();
 }
+
+function updateTodo($id, $duedate, $message) {
+    global $db;
+    date_default_timezone_set("America/New_York");
+    $datetime = date("Y-m-d H:i:sa");
+    $query = 'UPDATE todos SET createddate = :createddate, duedate = :duedate, message = :message WHERE id = :id';
+    $statement = $db->prepare($query);
+    $statement->bindValue(":createddate", $datetime);
+    $statement->bindValue(":duedate", $duedate);
+    $statement->bindValue(":message", $message);
+    $statement->bindValue(":id", $id);
+    $statement->execute();
+    $statement->closeCursor();
+}
