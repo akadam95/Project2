46c3e8d2c3037f12cdcf168eb579e2b319ea5401
ak985 <ak985@njit.edu>
2017-12-20 20:01:27 -0500

    added code from live code and edited credentials

---
+++ model/database.php
@@ -0,0 +1,12 @@
+<?php
+    $dsn = 'mysql:host=sql2.njit.edu;dbname=ak985';
+    $username = 'ak985';
+    $password = 'XwA5ITTI';
+    try {
+        $db = new PDO($dsn, $username, $password);
+    } catch (PDOException $e) {
+        $error_message = $e->getMessage();
+        include('../errors/database_error.php');
+        exit();
+    }
+?>
