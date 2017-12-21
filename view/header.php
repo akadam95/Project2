36f98ce66aebb1a944636190176c1a680da3ddce
ak985 <ak985@njit.edu>
2017-12-20 19:55:40 -0500

    added code from live code and edited

---
+++ view/header.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<!-- the head section -->
+<head>
+    <title>To Do Web App</title>
+    <link rel="stylesheet" type="text/css"
+          href="https://web.njit.edu/~ak985/Is218/Project2/mainstyle.css">
+</head>
+
+<!-- the body section -->
+<body>
+<header><h1>To do List</h1></header>
